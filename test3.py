e92386570aa2d23ff8c75c05fcc2c06e96c80f8d
Miguel-Escobar <miguelescobar@ug.uchile.cl>
2022-01-18 15:17:20 -0300

    Encuentra un caso especial del logistic map.

--- test3.py
+++ test3.py
@@ -1,12 +1,12 @@
 from ts2vg import HorizontalVG
 import numpy as np
 import matplotlib.pyplot as plt
-
+import networkx as nx
 q = 1.5
 r = np.linspace(1, 2, 10**4)
-N_values = 10**3
+N_values = 1000
 n_trans = 10**5
-
+scaling = 1
 def dist_degree_in(g):
 
     ks, counts = np.unique(g.degrees_in, return_counts=True)
@@ -43,8 +43,9 @@ suma = np.zeros(len(r))
 problems = []
 
 for k in range(len(r)):
+
     print(k)
-    g = HorizontalVG(directed='left_to_right').build(ts[:,k], only_degrees=True)
+    g = HorizontalVG(directed='left_to_right').build(ts[:,k])
 
     ks_in, ps_in = dist_degree_in(g)
     ks_out, ps_out = dist_degree_out(g)
@@ -58,7 +59,15 @@ for k in range(len(r)):
     
     if suma[k] < 1 or suma[k]>1.2:
         print('PROBLEM IN r=' + str(r[k]))
-        problems.append((k, ps_out, ps_in, suma[k]))
+        problems.append((k, ps_out, ps_in, suma[k], g))
+
+nxg = problems[0][4].as_networkx()
+nodepos = problems[0][4].node_positions()
+
+for i in range(N_values):
+    lst = list(nodepos[i])
+    lst[1] += -ts[0, problems[0][0]]
+    nodepos[i] = tuple(lst)
 
 LVD = (1 - 1/suma)/(q-1)
 RD = np.log(suma)/(q-1)
@@ -76,3 +85,19 @@ ax.set_xlabel('r')
 ax.set_ylabel(r'$D(P_{out}||P_{in})$')
 ax.legend()
 fig.show()
+
+fig2 = plt.figure(2)
+fig2.clf()
+ax2 = fig2.add_subplot(111)
+graph_plot_options = {
+    'with_labels': False,
+    'node_size': 2,
+    'node_color': [(0, 0, 0, 1)],
+    'edge_color': [(0, 0, 0, 0.15)],
+}
+nx.draw_networkx(nxg, ax=ax2, pos=nodepos, **graph_plot_options)
+ax2.tick_params(bottom=True, left=True, labelleft=True, labelbottom=True)
+ax2.plot((ts[:, problems[0][0]] - ts[0, problems[0][0]])*scaling)
+ax2.set_title('Time Series')
+ax2.set_ylim((-1e-15,1e-15))
+fig2.show()
